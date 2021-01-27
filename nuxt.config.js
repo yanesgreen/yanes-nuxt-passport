cd80d16010514fce84c4111a84dc19b67cdecff7
yulyanes - y540 <yoeker@gmail.com>
2021-01-27 13:54:11 +0700

    add vercel.json

--- nuxt.config.js
+++ nuxt.config.js
@@ -123,6 +123,7 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     extractCSS: true,
-    transpile: ['vuelidate']
+    transpile: ['vuelidate'],
+
   }
 }
